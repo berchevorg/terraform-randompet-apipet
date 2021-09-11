84a25c2128637a35406718b3390ff7476a205b26
Georgi Berchev a.k.a Georgiman <g.berchev@gmail.com>
2021-09-11 09:15:00 +0300

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,10 @@
+resource "random_pet" "name" {
+ length    = "2"
+ separator = "-"
+}
+
+resource "null_resource" "helloWorld" {
+  provisioner "local-exec" {
+    command = "echo ${random_pet.name.id}"
+  }
+}
